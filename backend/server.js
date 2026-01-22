fd56cbd75d1230e432748f1e72fb32617a049c89
Neha9430 <nehamakashre70@gmail.com>
2026-01-22 11:19:21 +0530

    Fix error

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://medical-prescription-reminder.onrender.com/",
+      "https://medical-reminder-frontend.onrender.com/",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
